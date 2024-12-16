b5d24bdcba546f36a8667abb2e075cb32bbb8adb
benik <benjamin.kohn@axon-pulse.com>
2024-12-16 16:09:00 +0200

    add test for gotch data module

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -1,9 +1,47 @@
+import os
+import sys
 from pathlib import Path
 
+import hydra
 import pytest
 import torch
+from hydra import compose, initialize
+from hydra.core.global_hydra import GlobalHydra
+from lightning import LightningDataModule
+from omegaconf import DictConfig, open_dict
 
+from src.datamodule.gotcha_PL_datamodule_v2 import GotchaDataModule
 from src.datamodule.mnist_datamodule import MNISTDataModule
+
+
+def test_gotcha_datamodule() -> None:
+    """Tests `GotchaDataModule` to verify that it can be downloaded correctly,
+    that the necessary attributes were created (e.g., the dataloader objects),
+    and that dtypes and batch sizes correctly match.
+    """
+    # Initialize Hydra and compose the configuration directly inside the test
+    with initialize(version_base="1.3", config_path="../configs/datamodule"):
+        cfg = compose(
+            config_name="gotcha_v2.yaml",
+            return_hydra_config=True,
+            overrides=[
+                "datasets.main.csv_path=tests/data/gotcha/gt_with_recs_stats_df_subset_for_test.csv",
+            ],
+        )
+    # Now use the configuration to test the data module
+    dm = GotchaDataModule(
+        batch_size=2,
+        num_workers=4,
+        pin_memory=False,
+        datasets=cfg.datasets,
+        train_val_split=[0.8, 0.2],
+    )
+
+    # Perform assertions or checks on the data module here
+    # Example: checking if the dataloaders were correctly created
+    dm.setup()
+    assert dm.train_dataloader() is not None
+    assert dm.val_dataloader() is not None
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
